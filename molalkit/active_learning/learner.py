6ea99a8c7da95f112c5bdc96ef8c0f24309a7f98
xiangyan93 <yan.xiang@duke.edu>
2025-08-18 10:29:37 -0400

    Add feature: When the same data point exists in both the training and test sets, its exact value is used in the evaluation step.

--- molalkit/active_learning/learner.py
+++ molalkit/active_learning/learner.py
@@ -197,6 +197,7 @@ class ActiveLearner:
             for i, model in enumerate(self.models):
                 model.fit_molalkit(self.datasets_train[i])
                 y_pred = model.predict_value(self.datasets_val[i])
+                y_pred = [data.targets[0] if data.uidx in alr.uidx_after else y_pred[i] for i, data in enumerate(self.datasets_val[i])]
                 if self.detail:
                     df = pd.DataFrame({"true": self.datasets_val[i].y.ravel(), "pred": y_pred})
                     df.to_csv(os.path.join(self.save_dir, f"model_{i}-iter_{self.current_iter}.csv"), index=False)
